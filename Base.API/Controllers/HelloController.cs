a0a9036ce79128c6a8d4c12d8b5ff22b966062cf
khoaLe12 <lek35067@gmail.com>
2024-05-08 20:23:35 +0700

    Add fingerprint testing

--- Base.API/Controllers/HelloController.cs
+++ Base.API/Controllers/HelloController.cs
@@ -6,9 +6,24 @@ namespace Base.API.Controllers;
 [Route("api/[controller]")]
 public class HelloController : ControllerBase
 {
+    private static List<string> fingerprintTemplates = new List<string>();
     [HttpGet]
     public IActionResult Hello()
     {
         return Ok("Hello");
+    }
+
+
+    [HttpPost("fingerprint")]
+    public IActionResult AddNew([FromBody] string fingerprintTemplate)
+    {
+        fingerprintTemplates.Add(fingerprintTemplate);
+        return Ok("Ok");
+    }
+
+    [HttpGet("fingerprint")]
+    public IActionResult GetAllFingerprintTemplates()
+    {
+        return Ok(fingerprintTemplates);
     }
 }
