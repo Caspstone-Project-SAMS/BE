00fe9afa0db5ea70557ad73b1e26ddbf1f8d9a0a
khoaLe12 <lek35067@gmail.com>
2024-11-06 17:05:41 +0700

    test CD

---
+++ Base.API/Controllers/TestCDController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Base.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestCDController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult TestCD()
+        {
+            return Ok();
+        }
+    }
+}
